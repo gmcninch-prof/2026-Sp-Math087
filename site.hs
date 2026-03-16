2be12f441da5f854c6862b7bf0c097aaebae178e
George McNinch <gmcninch@gmail.com>
2026-03-16 18:55:00 -0400

    json for savanna

--- site.hs
+++ site.hs
@@ -36,6 +36,7 @@ main = hakyllWith config $ do
     match ("course-content/*ipynb"
            .||. "course-content/*pdf"
            .||. "course-content/*py"
+           .||. "course-content/*json"
            .||. "course-assignments/**/*png") $ version "copy" $ do
         route   idRoute
         compile copyFileCompiler
